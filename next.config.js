6bbd9e30f77d504bc27bbf3fef07c026f7d45fd9
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2023-02-06 21:06:28 -0800

    updating project files

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["moviesapi.ir"],
+  },
+};
+
+module.exports = nextConfig;
